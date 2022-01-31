f91c6e58483f2b5652598fbe63b2ab6b7f0c3c89
Jennifer Lee <jelee14108@brandeis.edu>
2022-01-30 20:00:20 -0500

    Add EnvironmentalVariableKeeper

--- src/main/java/com/newscurator/app/EnvironmentVariableKeeper.java
+++ src/main/java/com/newscurator/app/EnvironmentVariableKeeper.java
@@ -1,2 +1,25 @@
-package com.newscurator.app;public class EnvironmentVariableKeeper {
+package com.newscurator.app;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class EnvironmentVariableKeeper {
+
+    private static EnvironmentVariableKeeper instance;
+
+    private Dotenv dotenv;
+
+    private EnvironmentVariableKeeper() {
+        dotenv = Dotenv.load();
+    }
+
+    public static EnvironmentVariableKeeper getInstance(){
+        if (instance == null){
+            instance = new EnvironmentVariableKeeper();
+        }
+        return instance;
+    }
+
+    public String getVariable(String variableName){
+        return dotenv.get(variableName);
+    }
 }
